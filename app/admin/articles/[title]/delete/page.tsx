6f00c3b5afa0174f1bde71cc4e5e42db69daa634
Eduardo <eduardoamdev@gmail.com>
2024-07-04 22:56:52 +0200

    Implementing delete

---
+++ app/admin/articles/[title]/delete/page.tsx
@@ -0,0 +1,27 @@
+import Navbar from "../../../../components/Navbar";
+import ArticleActions from "@/app/components/ArticleActions";
+import { NextPageContext } from "next";
+
+interface ArticleProps {
+  params: NextPageContext["query"];
+}
+
+export default function DeleteArticle({ params }: ArticleProps) {
+  return (
+    <main>
+      <Navbar authenticated="true" />
+      <ArticleActions title={params.title} />
+      <div className="text-white mt-20 flex flex-col items-center">
+        <div className="text-1xl md:text-1xl lg:text-2xl">Are you sure?</div>
+        <div className="mt-20">
+          <button className="bg-red-600 w-20 p-2 text-1xl md:text-1xl lg:text-2xl">
+            Yes
+          </button>
+          <button className="bg-gray-400 w-20 p-2 text-1xl md:text-1xl lg:text-2xl">
+            No
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
